c412fd9f4607f56b699cb875bc7d5a2691c577b1
ankitatripathi8176-hash <ankitatripathi8176@gmail.com>
2026-06-29 11:40:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("meraButton");
+let jawab = document.getElementById("jawab");
+
+button.onclick = function() {
+  jawab.innerText = "Haaan Ankita! Tu America zaroor jayegi! 👑✈️🇺🇸 Papa I did it!";
+  button.innerText = "Main Zinda Hu 🔥";
+  button.style.background = "#10b981";
+}
